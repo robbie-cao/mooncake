359b4d6830e5a0cfcb07391070979d5db6cd935a
Robbie Cao <robbie.cao@gmail.com>
2017-01-10 13:04:19 +0800

    Download with lcurl - v1
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- fdl.lua
+++ fdl.lua
@@ -1,24 +1,47 @@
 -- download file at the url
 
-url = arg[1]
+curl = require("lcurl")
+
+remote_url = arg[1]
 
 usage = [[
 url [path/to/save]
 ]]
 
-if (not url) then
+if not arg[1] or arg[1] == "" then
     print("URL cannot be nil\n")
     print("Usage:")
     print(arg[0], usage)
     os.exit()
 end
 
+remote_url = arg[1]
+
 path_to_save = os.date("%Y%m%d-%H%M%S")
 if (arg[2] and arg[2] ~= "") then
     path_to_save = arg[2]
 end
 
-print("Download: " .. url)
+f = assert(io.open(path_to_save, "w"))
+p = function ()
+    print("#")
+end
+
+c = curl.easy{
+    url = remote_url,
+    [curl.OPT_VERBOSE] = true,
+    [curl.OPT_NOPROGRESS] = false;
+}
+
+c:setopt_writefunction(f)
+c:setopt_progressfunction(p)
+
+print("Download: " .. remote_url)
 -- curl URL -o path/to/save
-os.execute("curl " .. tostring(url) .. " -o " .. path_to_save)
+--os.execute("curl " .. tostring(url) .. " -o " .. path_to_save)
+c:perform()
 print("Saved to: " .. path_to_save)
+
+c:close()
+f:close()
+print("Done")
