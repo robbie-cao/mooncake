006b20daf518ee3c7e064bec4fe70bf16f5e62d4
Robbie Cao <robbie.cao@gmail.com>
2017-01-13 16:25:53 +0800

    Add comments
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- mos-sub.lua
+++ mos-sub.lua
@@ -12,16 +12,19 @@ local MSM_TOPIC_DOMAIN            = "/mua/msm/"
 local MSM_DEFAULT_TOPIC_NAME      = "/mua/msm/general"
 local MSM_DEFAULT_TEST_TOPIC_NAME = "/mua/msm/general_test"
 
+-- Use first arg as tag, use current time HHMMSS as default
 tag = arg[1]
 
 if not tag or tag == "" then
     tag = tostring(os.date("%H%M%S"))
 end
 
+-- Save standard print to P
 P = print
+-- Customized print(): add timestamp and tag before content
 local function print(...)
     t = posix.gettimeofday()
-    P(string.format("%08d.%06d", t.sec, t.usec), tag, ...)
+    P(string.format("%08d.%06d", t.sec, t.usec), tag, ...)  -- Use t.tv_sec / t.tv_usec in old version of luaposix
 end
 
 client.ON_CONNECT = function(...)
