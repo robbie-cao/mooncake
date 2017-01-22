cd879ca9fb50044e51c08647f457a15c2308d0a1
Robbie Cao <robbie.cao@gmail.com>
2017-01-22 17:15:22 +0800

    Retrieve added/removed file node name
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- fs-detect.lua
+++ fs-detect.lua
@@ -8,8 +8,10 @@ local path = arg[1] or "."
 
 local fdir = posix.dir(path)
 local fset = Set(fdir)
+--[[
 pretty.dump(fdir)
 pretty.dump(fset)
+--]]
 
 local fse = uv.new_fs_event()
 assert(uv.fs_event_start(fse, path, {
@@ -20,22 +22,38 @@ assert(uv.fs_event_start(fse, path, {
     if err then
         print("Error " .. err)
     else
+        --[[
         print(string.format("Change detected in %s", uv.fs_event_getpath(fse)))
+        --]]
         if status["change"] == true then
+            --[[
             print("change: " .. (fname and fname or ""))
+            --]]
         elseif status["rename"] == true then
+            --[[
             print("rename: " .. (fname and fname or ""))
+            --]]
             fdir2 = posix.dir(path)
             fset2 = Set(fdir2)
             removed = fset - fset2
             added = fset2 - fset
             if #removed > 0 then
                 print("Node removed:")
+                --[[
                 pretty.dump(removed)
+                --]]
+                for i = 1, #removed do
+                    print(Set.values(removed)[i])
+                end
             end
             if #added > 0 then
                 print("Node added:")
+                --[[
                 pretty.dump(added)
+                --]]
+                for i = 1, #added do
+                    print(Set.values(added)[i])
+                end
             end
             -- Store current file nodes for next comparison
             fset = fset2
